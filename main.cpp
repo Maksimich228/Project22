08a0d86dbb9a17d8d618c044dfcec6c9c399673a
Maksimich228 <147976922+Maksimich228@users.noreply.github.com>
2024-08-24 11:37:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <mutex>
+
 struct Node {
     int value;
     Node* next;
     std::mutex* node_mutex;
 };
+
 class FineGrainedQueue {
     Node* head;
     std::mutex* queue_mutex;
+
 public:
     FineGrainedQueue() : head(nullptr), queue_mutex(new std::mutex()) {}
+
     void insertIntoMiddle(int value, int pos) {
         Node* newNode = new Node{ value, nullptr, new std::mutex() };
         queue_mutex->lock();
+
         Node* current = head;
         Node* prev = nullptr;
         int currentPos = 0;
+
+        if (current != nullptr) {
+            current->node_mutex->lock();
+        }
+
         while (current != nullptr && currentPos < pos) {
             if (prev != nullptr) {
                 prev->node_mutex->unlock();
@@ -22,20 +32,24 @@ public:
             prev = current;
             current = current->next;
             currentPos++;
-\            current->node_mutex->lock();
+
+            if (current != nullptr) {
+                current->node_mutex->lock();
+            }
         }
+
         newNode->next = current;
+
         if (prev != nullptr) {
             prev->next = newNode;
-            prev->node_mutex->unlock(); 
+            prev->node_mutex->unlock();
         }
         else {
-            
             head = newNode;
         }
 
         if (current != nullptr) {
-            current->node_mutex->unlock(); 
+            current->node_mutex->unlock();
         }
 
         queue_mutex->unlock();
